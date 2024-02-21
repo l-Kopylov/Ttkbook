7e1d3c59ef00ae4aaaabef3ef85cd2261ea3e9f5
l-Kopylov <qazar7@yandex.ru>
2024-02-21 23:50:36 +0300

    migration

---
+++ database/migrations/2024_02_21_202330_create_books_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('bname', 150);
+            $table->dateTime('bdate');
+            $table->text('bdescription', 500);
+            $table->string('bcover', 150);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
