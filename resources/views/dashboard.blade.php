ec0f03a48981f2667e6512288c501c5dcd808e23
l-Kopylov <qazar7@yandex.ru>
2024-02-24 00:53:25 +0300

    Форма добавления сущности

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,11 +31,15 @@
                         font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
                     Добавить рубрику
                     </button><br />
-                    <button type="button" class="text-blue bg-gradient-to-r from-green-400 via-green-500 to-green-600 
+                    <button  href="{{ route('add-book') }}" type="button"  class="text-blue bg-gradient-to-r from-green-400 via-green-500 to-green-600 
                         hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium 
                         rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                        
                     Добавить книгу
                     </button><br />
+                   
+                    <a href="{{ route('add-book') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white 
+                        focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Добавить книгу</a>
                      
                     <div class="sm:fixed sm:top-0 sm:left-0 p-6 text-right z-10">
                         <a href="{{ url('/') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white 
