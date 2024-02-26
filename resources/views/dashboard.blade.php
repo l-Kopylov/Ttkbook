7ff64486f23cb13d6cd721183077ee81f58d361c
l-Kopylov <qazar7@yandex.ru>
2024-02-26 17:57:53 +0300

    фикс до рабочего состояния

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,6 +21,11 @@
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                     @auth
+                    <div>{{ Auth::user()->name }}</div>
+                    <div class="px-4">
+                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
+                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+            </div>
                     <button type="button" class="text-blue bg-gradient-to-r from-green-400 via-green-500 to-green-600 
                         hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium 
                         rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
